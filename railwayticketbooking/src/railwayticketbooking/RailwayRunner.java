9a214d3acfda77571423fa91ca419b9cd128d4af
suryaraj086 <79685586+suryaraj086@users.noreply.github.com>
2022-05-02 14:51:16 +0530

    Add files via upload

---
+++ railwayticketbooking/src/railwayticketbooking/RailwayRunner.java
@@ -0,0 +1,101 @@
+package railwayticketbooking;
+
+import java.util.Scanner;
+
+public class RailwayRunner {
+
+	public static void main(String[] args) {
+		RailwayTicket obj = new RailwayTicket();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the slots");
+		int book = scan.nextInt();
+		int rac = scan.nextInt();
+		int waiting = scan.nextInt();
+		obj.berthCreation(book, rac, waiting);
+		boolean bool = true;
+		while (bool) {
+			System.out.println(
+					"1.Book Ticket\n2.Cancel Ticket \n3.Cancel person\n4.Print Booked Tickets\n5.Print available tickets");
+			int choice = scan.nextInt();
+			switch (choice) {
+			case 1:
+				int id = obj.createId();
+				System.out.println("Enter the number of passengers");
+				int passNumber = scan.nextInt();
+				for (int i = 0; i < passNumber; i++) {
+					System.out.println("1.Upper berth\n2.Middle berth\n3.Lower berth");
+					System.out.println("Enter the berth type");
+					int val = scan.nextInt();
+					BerthType type = null;
+					switch (val) {
+					case 1: {
+						type = BerthType.Upper;
+						break;
+					}
+					case 2: {
+						type = BerthType.Middle;
+						break;
+					}
+					case 3: {
+						type = BerthType.Lower;
+						break;
+					}
+					}
+					scan.nextLine();
+					System.out.println("Enter the name");
+					String name = scan.nextLine();
+					System.out.println("Enter the age");
+					int age = scan.nextInt();
+					Passenger pass = ObjectSetter.passengerSetter(name, id, age);
+					System.out.println("Passenger id " + id);
+					try {
+						System.out.println(obj.bookBerth(pass, id, type));
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+				}
+				break;
+			case 2:
+				System.out.println("Enter the ticket id to cancel");
+				int passid = scan.nextInt();
+				try {
+					System.out.println(obj.cancelTicket(passid));
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case 3:
+				System.out.println("Enter the ticket id to cancel");
+				int passid1 = scan.nextInt();
+				System.out.println("Enter the number of persons to cancel");
+				int loop = scan.nextInt();
+				for (int i = 0; i < loop; i++) {
+					try {
+						System.out.println(obj.ticketPersons(passid1));
+					} catch (Exception e1) {
+						System.out.println(e1.getMessage());
+					}
+					System.out.println("Enter the person number you want to delete");
+					int val = scan.nextInt();
+					try {
+						System.out.println(obj.cancelIndividual(passid1, val));
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+				}
+				break;
+			case 4:
+				System.out.println(obj.showTicket());
+				break;
+			case 5:
+				System.out.println(obj.availableTicket());
+				break;
+
+			default:
+				break;
+			}
+		}
+		scan.close();
+	}
+
+}
---
+++ railwayticketbooking/src/railwayticketbooking/RailwayTicket.java
@@ -0,0 +1,293 @@
+package railwayticketbooking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class RailwayTicket {
+	int ticketid = 0;
+	int passengerid = 0;
+	Map<BerthType, List<Berth>> berth = new HashMap<>();
+	Map<Integer, List<Passenger>> ticket = new HashMap<>();
+	Map<Integer, List<Berth>> bookedslots = new HashMap<>();
+	Map<String, List<Passenger>> rwTickets = new HashMap<>();
+
+	int totalBerth = 0;
+	int totalRac = 0;
+	int totalWaiting = 0;
+
+	public String berthCreation(int berthNum, int rac, int waiting) {
+		this.totalBerth = berthNum;
+		this.totalRac = rac;
+		this.totalWaiting = waiting;
+		int k = 1;
+		List<Berth> list = new ArrayList<Berth>();
+		for (int i = 1; i <= berthNum / 3; i++) {
+			Berth berObj = new Berth();
+			berObj.setSeatNo(k++);
+			berObj.setType(BerthType.Upper);
+			list.add(berObj);
+		}
+		berth.put(BerthType.Upper, list);
+		list = new ArrayList<Berth>();
+		for (int i = 1; i <= berthNum / 3; i++) {
+			Berth berObj = new Berth();
+			berObj.setSeatNo(k++);
+			berObj.setType(BerthType.Middle);
+			list.add(berObj);
+		}
+		berth.put(BerthType.Middle, list);
+		list = new ArrayList<Berth>();
+		for (int i = 1; i <= berthNum / 3; i++) {
+			Berth berObj = new Berth();
+			berObj.setSeatNo(k++);
+			berObj.setType(BerthType.Lower);
+			list.add(berObj);
+		}
+		berth.put(BerthType.Lower, list);
+		rwTickets.put("RAC", new ArrayList<Passenger>());
+		rwTickets.put("Waiting", new ArrayList<Passenger>());
+		return "creation succesful";
+	}
+
+	public String bookBerth(Passenger info, int id, BerthType type) throws Exception {
+		List<Passenger> arrList = ticket.get(id);
+		if (arrList == null) {
+			arrList = new ArrayList<Passenger>();
+			ticket.put(id, arrList);
+			info.setStatus("Booked");
+		}
+		info.setStatus("Booked");
+		arrList.add(info);
+		List<Berth> book = bookedslots.get(id);
+		if (book == null) {
+			book = new ArrayList<Berth>();
+			bookedslots.put(id, book);
+		}
+		if (berth.get(type).size() == 0) {
+			if (berth.get(BerthType.Upper).size() != 0) {
+				arrList.remove(arrList.size() - 1);
+				return bookBerth(info, id, BerthType.Upper);
+			} else if (berth.get(BerthType.Middle).size() != 0) {
+				arrList.remove(arrList.size() - 1);
+				return bookBerth(info, id, BerthType.Middle);
+			} else if (berth.get(BerthType.Lower).size() != 0) {
+				arrList.remove(arrList.size() - 1);
+				return bookBerth(info, id, BerthType.Lower);
+			} else {
+				if (rwTickets.get("RAC").size() < totalRac) {
+					arrList = rwTickets.get("RAC");
+					arrList.add(info);
+					info.setStatus("RAC");
+					return "RAC Booked";
+				} else if (rwTickets.get("RAC").size() >= totalRac && rwTickets.get("Waiting").size() < totalWaiting) {
+					arrList = rwTickets.get("Waiting");
+					arrList.add(info);
+					info.setStatus("Waiting");
+					return "Waiting booked";
+				} else {
+					ticket.remove(id);
+					return "No tickets available";
+				}
+			}
+		}
+		Berth berthObj = berth.get(type).get(0);
+		berth.get(type).remove(0);
+		book.add(berthObj);
+		info.setSeatNo(berthObj.getSeatNo());
+		return "booked successfully and seat number is " + berthObj.getSeatNo();
+	}
+
+	public String showTicket() {
+		String out = "";
+		Set<Integer> keys = ticket.keySet();
+		for (int key : keys) {
+			out += ticket.get(key).toString();
+		}
+		return out;
+	}
+
+	public String availableTicket() {
+		String avail = "";
+		avail += "Upper berth " + berth.get(BerthType.Upper).size() + "\n";
+		avail += "Middle berth " + berth.get(BerthType.Middle).size() + "\n";
+		avail += "Lower berth " + berth.get(BerthType.Lower).size() + "\n";
+		avail += "RAC " + (totalRac - rwTickets.get("RAC").size()) + "\n";
+		avail += "Waiting " + (totalWaiting - rwTickets.get("Waiting").size()) + "\n";
+		return avail;
+	}
+
+	public String cancelIndividual(int id, int val) throws Exception {
+		List<Passenger> pass = ticket.get(id);
+		int size = 1;
+		nullChecker(pass);
+		String status = ticket.get(id).get(val).getStatus();
+		List<Berth> slots = bookedslots.get(id);
+		pass.remove(val);
+		if (status.equals("Booked")) {
+			for (int i = 0; i < size; i++) {
+				List<Passenger> rac = rwTickets.get("RAC");
+				List<Passenger> wait = rwTickets.get("Waiting");
+				if (rac.size() != 0 && rac.get(0).passengerid != id && rac.get(0).getStatus().equals("RAC")
+						&& rac.get(0) != null) {
+					Passenger obj = rac.get(0);
+					rac.remove(0);
+					obj.setStatus("Booked");
+					List<Berth> arr = bookedslots.get(obj.getPassengerid());
+					if (bookedslots.get(obj.getPassengerid()) == null) {
+						arr = new ArrayList<Berth>();
+					}
+					arr.add(slots.get(0));
+					obj.setSeatNo(slots.get(0).getSeatNo());
+					bookedslots.put(obj.getPassengerid(), arr);
+					if (wait.size() != 0) {
+						Passenger obj1 = wait.get(0);
+						wait.remove(0);
+						obj1.setStatus("RAC");
+						List<Passenger> arr1 = rwTickets.get("RAC");
+						arr1.add(obj1);
+					}
+					slots.remove(0);
+					pass.remove(0);
+				} else {
+					if (rac.size() == 0) {
+						List<Berth> list = berth.get(slots.get(0).getType());
+						list.add(slots.get(0));
+						berth.put(slots.get(0).getType(), list);
+						slots.remove(0);
+					}
+				}
+			}
+			return "removed";
+		} else if (status.equals("RAC")) {
+			List<Passenger> rac = rwTickets.get("RAC");
+			List<Passenger> wait = rwTickets.get("Waiting");
+			ticket.remove(id);
+			for (int i = 0; i < size; i++) {
+				if (wait.size() != 0 && wait.get(0).getPassengerid() != id && wait.get(0).getStatus().equals("Waiting")
+						&& wait.get(0) != null) {
+					Passenger obj1 = wait.get(0);
+					wait.remove(0);
+					obj1.setStatus("RAC");
+					List<Passenger> arr1 = rwTickets.get("RAC");
+					arr1.add(obj1);
+				}
+			}
+			for (int i = 0; i < rac.size(); i++) {
+				Passenger obj = rac.get(i);
+				if (obj.getPassengerid() == id) {
+					rac.remove(i);
+				}
+			}
+			return "removed";
+		} else {
+			List<Passenger> waiting = rwTickets.get("Waiting");
+			ticket.remove(id);
+			for (int i = 0; i < waiting.size(); i++) {
+				if (waiting.get(i).getPassengerid() == id) {
+					waiting.remove(i);
+					return "removed";
+				}
+			}
+		}
+		return "not found";
+	}
+
+	public String cancelTicket(int id) throws Exception {
+		List<Passenger> pass = ticket.get(id);
+		nullChecker(pass);
+		int size = ticket.get(id).size();
+		String status = ticket.get(id).get(0).getStatus();
+		List<Berth> slots = bookedslots.get(id);
+		ticket.remove(id);
+		if (status.equals("Booked")) {
+			for (int i = 0; i < size; i++) {
+				List<Passenger> rac = rwTickets.get("RAC");
+				List<Passenger> wait = rwTickets.get("Waiting");
+				if (rac.size() != 0 && rac.get(0).passengerid != id && rac.get(0).getStatus().equals("RAC")
+						&& rac.get(0) != null) {
+					Passenger obj = rac.get(0);
+					rac.remove(0);
+					obj.setStatus("Booked");
+					List<Berth> arr = bookedslots.get(obj.getPassengerid());
+					if (bookedslots.get(obj.getPassengerid()) == null) {
+						arr = new ArrayList<Berth>();
+					}
+					arr.add(slots.get(0));
+					obj.setSeatNo(slots.get(0).getSeatNo());
+					bookedslots.put(obj.getPassengerid(), arr);
+					if (wait.size() != 0) {
+						Passenger obj1 = wait.get(0);
+						wait.remove(0);
+						obj1.setStatus("RAC");
+						List<Passenger> arr1 = rwTickets.get("RAC");
+						arr1.add(obj1);
+					}
+					slots.remove(0);
+					pass.remove(0);
+				} else {
+					if (rac.size() == 0) {
+						List<Berth> list = berth.get(slots.get(0).getType());
+						list.add(slots.get(0));
+						berth.put(slots.get(0).getType(), list);
+						slots.remove(0);
+					}
+				}
+			}
+			return "removed";
+		} else if (status.equals("RAC")) {
+			List<Passenger> rac = rwTickets.get("RAC");
+			List<Passenger> wait = rwTickets.get("Waiting");
+			ticket.remove(id);
+			for (int i = 0; i < size; i++) {
+				if (wait.size() != 0 && wait.get(0).getPassengerid() != id && wait.get(0).getStatus().equals("Waiting")
+						&& wait.get(0) != null) {
+					Passenger obj1 = wait.get(0);
+					wait.remove(0);
+					obj1.setStatus("RAC");
+					List<Passenger> arr1 = rwTickets.get("RAC");
+					arr1.add(obj1);
+				}
+			}
+			for (int i = 0; i < rac.size(); i++) {
+				Passenger obj = rac.get(i);
+				if (obj.getPassengerid() == id) {
+					rac.remove(i);
+				}
+			}
+			return "removed";
+		} else {
+			List<Passenger> waiting = rwTickets.get("Waiting");
+			ticket.remove(id);
+			for (int i = 0; i < waiting.size(); i++) {
+				if (waiting.get(i).getPassengerid() == id) {
+					waiting.remove(i);
+					return "removed";
+				}
+			}
+		}
+		return "not found";
+	}
+
+	public void nullChecker(Object val) throws Exception {
+		if (val == null) {
+			throw new Exception("Failed");
+		}
+	}
+
+	public String ticketPersons(int id) throws Exception {
+		List<Passenger> list = ticket.get(id);
+		nullChecker(list);
+		String out = "";
+		for (int i = 0; i < list.size(); i++) {
+			out += i + " " + list.get(i);
+		}
+		return out;
+	}
+
+	public int createId() {
+		return ticketid++;
+	}
+}
